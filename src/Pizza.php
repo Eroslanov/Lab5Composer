ee4d3edc158c16642c029d1cb9791a0caf32471b
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-05-22 18:26:39 +0300

    Update Pizza.php

--- src/Pizza.php
+++ src/Pizza.php
@@ -4,24 +4,24 @@ namespace pizza;
 
 class Pizza
 {
-    private $name;
-    private $sauce;
-    private $toppings;
+    private string $name;
+    private string $sauce;
+    private array $toppings;
 
-    public function __construct($name, $sauce, $toppings)
+    public function __construct(string $name, string $sauce, array $toppings)
     {
         $this->name = $name;
         $this->sauce = $sauce;
         $this->toppings = $toppings;
     }
 
-    public function prepare()
+    public function prepare(): void
     {
         echo "Готовим пиццу {$this->name} с {$this->sauce} соусом и топпингами: {$this->toppings}\n";
-    
-    }
-    public function cut() {
-        echo "Данную пиццу нарезают по диагонали\n";
     }
 
+    public function cut(): void
+    {
+        echo "Данную пиццу нарезают по диагонали\n";
+    }
 }
